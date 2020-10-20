9c0dd79dd99603b4690a24a1bce04d914a372fb1
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-10-20 10:59:04 +0200

    IdleRpg Gold button
    +Unity UI Pro
    +Gold generator script

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Gold : MonoBehaviour {
+    int gold;
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+    public void AddGold () {
+        gold += 5;
+        GetComponent<TMP_Text> ().text = gold.ToString ("Gold: 0");
+    }
+
+}
