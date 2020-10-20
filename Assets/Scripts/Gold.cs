ea694eb113b31ee9b5f000c044232bb4afcd27d9
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-10-20 12:08:56 +0200

    IdleRpg buying goldMiners
    +Player can now spend gold on gold miners.
    +Changed structure on the gold gameobject.
    +Added gold and gold miner prefab

--- Assets/Scripts/Gold.cs
+++ Assets/Scripts/Gold.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 
 public class Gold : MonoBehaviour {
     public int goldEarnedByClick = 5;
+    public TMP_Text goldText;
     int gold;
 
     void Start () {
         gold = PlayerPrefs.GetInt ("savedGold");
-        GetComponent<TMP_Text> ().text = gold.ToString ("Gold: 0");
+        goldText.text = gold.ToString ("Gold: 0");
     }
-    public void AddGold () {
+    public int CurrentGold { get => gold; }
+    public void ProduceGold () {
         gold += goldEarnedByClick;
-        GetComponent<TMP_Text> ().text = gold.ToString ("Gold: 0");
+        UpdateGold ();
+    }
+    public void SpendGold (int cost) {
+        gold -= cost;
+        UpdateGold ();
+    }
+    void UpdateGold () {
+        goldText.text = gold.ToString ("Gold: 0");
         PlayerPrefs.SetInt ("savedGold", gold);
     }
 
---
+++ Assets/Scripts/GoldMiner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GoldMiner : MonoBehaviour {
+
+    int goldMiners;
+    public int price = 100;
+    public TMP_Text goldMinerTxt;
+    void Start () {
+        goldMiners = PlayerPrefs.GetInt ("savedMiners");
+        goldMinerTxt.text = goldMiners.ToString ("Gold miners: 0");
+    }
+    void Update () {
+        if (goldMiners > 0) {
+            MiningGold ();
+        }
+    }
+    public void BuyItem () {
+        if (FindObjectOfType<Gold> ().CurrentGold >= price) {
+            FindObjectOfType<Gold> ().SpendGold (price);
+            AddGoldMiner ();
+        }
+
+    }
+    public int GoldMiners { get => goldMiners; }
+    public void AddGoldMiner () {
+        goldMiners++;
+        goldMinerTxt.text = goldMiners.ToString ("Gold miners: 0");
+        PlayerPrefs.SetInt ("savedMiners", goldMiners);
+    }
+    void MiningGold () {
+
+    }
+}
