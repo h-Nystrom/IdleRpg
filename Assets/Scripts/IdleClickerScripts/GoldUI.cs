c685a03e70056c475acd176a7bd91908926454d4
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-10-23 20:36:32 +0200

    IdleRPG UI prototyping
    +Prototype for ui

--- Assets/Scripts/IdleClickerScripts/GoldUI.cs
+++ Assets/Scripts/IdleClickerScripts/GoldUI.cs
@@ -2,10 +2,10 @@
 
 public class GoldUI : MonoBehaviour {
     public GameObject goldIndicator;
-    public Transform uiLayer;
+    public Transform ignoreRaycast;
     public void SpawnGoldText (Vector3 position, int amountOfGold, bool lose) {
 
-        var newGoldText = Instantiate (goldIndicator, position, Quaternion.identity, uiLayer);
+        var newGoldText = Instantiate (goldIndicator, position, Quaternion.identity, ignoreRaycast);
         newGoldText.GetComponentInChildren<TMPro.TMP_Text> ().text = amountOfGold.ToString ("0g");
         newGoldText.GetComponentInChildren<Animator> ().SetBool ("Lose", lose);
     }
